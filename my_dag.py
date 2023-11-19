f36ab525a2893e06a8856064634ea55b8bf383ec
lucasljm2001 <lucas.martinez@naranjax.com>
2023-11-19 19:42:38 -0300

    creo dag

---
+++ my_dag.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="mi_ejemplo_bash",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["example", "example2"],
+    params={"example_key": "example_value"},
+) as dag:
+    run_this_last = EmptyOperator(
+        task_id="run_this_last",
+    )
+
+    # [START howto_operator_bash]
+    run_this = BashOperator(
+        task_id="run_after_loop",
+        bash_command='echo "hello world"',
+    )
+    # [END howto_operator_bash]
+
+    run_this >> run_this_last
+
+    # [START howto_operator_bash_template]
+    also_run_this = BashOperator(
+        task_id="also_run_this",
+        bash_command='echo "ti_key={{ task_instance_key_str }}"',
+    )
+    # [END howto_operator_bash_template]
+    also_run_this >> run_this_last
+
+
+if __name__ == "__main__":
+    dag.test()
